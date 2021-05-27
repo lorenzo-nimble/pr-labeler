d64414f2f6fb5059ba2f418505364ba6dc8ea0e4
Lorenzo <lorenzo@Lorenzos-MacBook-Pro.local>
2021-05-27 06:50:45 -0300

    fix

--- main.js
+++ main.js
@@ -12,15 +12,13 @@ let baseBranch = pullRequest.base.ref;
 let repoToken = core.getInput("repo-token", { required: true });
 let octokit = github.getOctokit(repoToken);
 
-let context = { github };
-
 switch(baseBranch){
     case "master": {
-        octokit.rest.issues.addLabels({ issue_number: context.payload.pull_request.number, owner: context.owner, repo: context.repo, labels: ['PROD'] });
+        octokit.rest.issues.addLabels({ issue_number: pullRequest.number, owner: context.owner, repo: context.repo, labels: ['PROD'] });
         break;
     }
     case "staging": {
-        octokit.rest.issues.addLabels({ issue_number: context.payload.pull_request.number, owner: context.owner, repo: context.repo, labels: ['STG'] });
+        octokit.rest.issues.addLabels({ issue_number: pullRequest.number, owner: context.owner, repo: context.repo, labels: ['STG'] });
         break;
     }
 }
