407e0ecbffb84c91a5c4f3a7a2b871f4fe5f9479
Lorenzo <lorenzo@Lorenzos-MacBook-Pro.local>
2021-05-27 06:35:23 -0300

    removed new

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ if (!pullRequest) {
 let baseBranch = pullRequest.base.ref;
 
 let repoToken = core.getInput("repo-token", { required: true });
-let octokit = new github.GitHub(repoToken);
+let octokit = github.GitHub(repoToken);
 
 let context = { github };
 
